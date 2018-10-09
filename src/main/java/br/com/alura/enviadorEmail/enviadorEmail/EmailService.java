39a4776fb42eb607e96f41ab7d9fb31cfb57fabb
Amauri Junior <ajunior@coddera.com>
2018-10-09 16:41:29 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/enviadorEmail/enviadorEmail/EmailService.java
@@ -0,0 +1,38 @@
+package br.com.alura.enviadorEmail.enviadorEmail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	private final String meuEmail = "meuEmail@teste.com";
+	
+	private final String senha = "123456";
+	private final String assunto = "Bem vindo";
+	
+	public void enviar(String nome, String emailConvidado) {
+		
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator(meuEmail, senha));
+            email.setSSLOnConnect(true);
+
+            email.setFrom(meuEmail);
+            email.setSubject(assunto);
+            email.setMsg("Olá " + nome + ". Você acaba de ser convidado pelo ListaVIP.");
+            email.addTo(emailConvidado);
+            email.send();
+
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
+    
+	}
+
+}
